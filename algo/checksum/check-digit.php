dc90b3186cd2694b192f7c5904fc98b7ace4449a
Fahmi Adam <mohammad@fahmi.my>
2018-05-08 17:24:52 +0800

    make sure result is single digit

--- algo/checksum/check-digit.php
+++ algo/checksum/check-digit.php
@@ -1,25 +1,29 @@
 <?php
-$num = 543215432154321;
-
 function checkDigit($num) {
-  $a = [];
-  $b = [];
-  $c = [];
+    $a = [];
+    $b = [];
+    $c = [];
   
-
-  $char = str_split((string) $num);
-  for ($i = 0; $i < count($char); $i+=3) {
-      $a[] = $char[$i] * 3;
-      $b[] = $char[$i+1] * 5;
-      $c[] = $char[$i+2] * 7;
-  }
-  
-  $sum = 0;
-  foreach([$a, $b, $c] as $ph) {
-      $sum += array_sum($ph);
+    $char = str_split((string) $num);
+    for ($i = 0; $i < count($char); $i+=3) {
+        $a[] = $char[$i] * 3;
+        $b[] = $char[$i+1] * 5;
+        $c[] = $char[$i+2] * 7;
     }
   
-  return array_sum(str_split((string) $sum));
+    $sum = 0;
+    foreach([$a, $b, $c] as $ph) {
+        $sum += array_sum($ph);
+    }
+
+    do {
+        $sum = array_sum(str_split((string) $sum));
+        if ($sum < 10) {
+            break;
+        }
+    } while(1);
+
+    return $sum;
 }
 
-echo checkDigit($num);
+echo checkDigit(543215432154321); // 9
